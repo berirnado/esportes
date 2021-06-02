01a628ad01d8f4ca2ae71cb8742165f241fc9f43
berirnado <be.zito@hotmail.com>
2021-06-02 00:31:12 -0300

    agora acho que foi

--- src/routes/usuario.js
+++ src/routes/usuario.js
@@ -4,17 +4,19 @@ const router = express.Router()
 
 const usuarioController = require('../controller/usuario')
 
+const upload = require('../config/multer')
+
 router.get('/', usuarioController.list)
 
 router.post('/', usuarioController.filtro)
 
 router.get('/add', usuarioController.abreadd)
 
-router.post('/add', usuarioController.add)
+router.post('/add', upload.Single('foto'), usuarioController.add)
 
 router.get('/edit/:id', usuarioController.abreedit)
 
-router.post('/edit/:id', usuarioController.edit)
+router.post('/edit/:id', upload.Single('foto'),  usuarioController.edit)
 
 router.get('/del/:id', usuarioController.del)
 
